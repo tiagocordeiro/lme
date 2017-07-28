bb176f4ca603898cc5c974b76d5f40fb075aa5e4
mulhergorila <tiago@mulhergorila.com>
2017-07-28 14:42:10 -0300

    Nova função que converte as cotações de CSV para json

---
+++ lme/csv2json.py
@@ -0,0 +1,10 @@
+import csv
+import json
+
+def converte_csv2json():
+    with open('cotacoes/cotacao-atual.csv') as f:
+        reader = csv.DictReader(f)
+        rows = list(reader)
+
+    with open('cotacoes/cotacao-atual.json', 'w') as f:
+        json.dump(rows, f)
