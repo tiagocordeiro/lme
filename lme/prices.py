c60d35e3f079ef1dee5d10c074ade68423c9a1b2
Tiago Cordeiro <tiago@mulhergorila.com>
2021-10-13 10:35:45 -0300

    Refactoring
    Melhora legibilidade do código e adequa a PEP8

--- lme/prices.py
+++ lme/prices.py
@@ -6,14 +6,22 @@ from rich.console import Console
 from rich.table import Table
 
 
+def media(valor, dias):
+    return str(round(Decimal(valor / dias), ndigits=2))
+
+
 def get_prices():
-    response = requests.get("https://lme.gorilaxpress.com/cotacao/2cf4ff0e-8a30-48a5-8add-f4a1a63fee10/json/")
+    url_base = "https://lme.gorilaxpress.com/cotacao"
+    data_link = "2cf4ff0e-8a30-48a5-8add-f4a1a63fee10/json"
+
+    response = requests.get(f"{url_base}/{data_link}/")
 
     prices = response.json()["results"]
 
     title_icon = "[not italic]:chart_with_upwards_trend:[/]"
+    title_text = "Cotação London Metal Exchange"
 
-    table = Table(title=f"{title_icon} Cotação London Metal Exchange {title_icon}")
+    table = Table(title=f"{title_icon} {title_text} {title_icon}")
 
     table.add_column("Data", justify="center", style="cyan", no_wrap=True)
     table.add_column("Zinco", justify="center", style="magenta")
@@ -24,52 +32,54 @@ def get_prices():
     table.add_column("Níquel", justify="center", style="bright_yellow")
     table.add_column("Dolar", justify="center", style="green")
 
-    ultima_semana = datetime.strptime(prices[0]["data"], "%Y-%m-%d").strftime("%U")
-    semana_dias = 0
-    media_semana_zinco = 0
-    media_semana_cobre = 0
-    media_semana_aluminio = 0
-    media_semana_chumbo = 0
-    media_semana_estanho = 0
-    media_semana_niquel = 0
-    media_semana_dolar = 0
+    ultima_semana = datetime.strptime(prices[0]["data"], "%Y-%m-%d").strftime(
+        "%U")
+    dias = 0
+    media_zinco = 0
+    media_cobre = 0
+    media_aluminio = 0
+    media_chumbo = 0
+    media_estanho = 0
+    media_niquel = 0
+    media_dolar = 0
 
     for price in prices:
-        _data = datetime.strptime(price["data"], "%Y-%m-%d").strftime("%d/%m/%Y")
+        dia = datetime.strptime(price["data"], "%Y-%m-%d").strftime("%d/%m/%Y")
 
-        semana_numero = datetime.strptime(_data, "%d/%m/%Y").strftime("%U")
+        semana_numero = datetime.strptime(dia, "%d/%m/%Y").strftime("%U")
 
         if semana_numero != ultima_semana:
             table.add_row(f"Média Semana {int(semana_numero) - 1}",
-                          str(round(Decimal(media_semana_zinco / semana_dias), ndigits=2)),
-                          str(round(Decimal(media_semana_cobre / semana_dias), ndigits=2)),
-                          str(round(Decimal(media_semana_aluminio / semana_dias), ndigits=2)),
-                          str(round(Decimal(media_semana_chumbo / semana_dias), ndigits=2)),
-                          str(round(Decimal(media_semana_estanho / semana_dias), ndigits=2)),
-                          str(round(Decimal(media_semana_niquel / semana_dias), ndigits=2)),
-                          str(round(Decimal(media_semana_dolar / semana_dias), ndigits=2)))
+                          media(media_zinco, dias),
+                          media(media_cobre, dias),
+                          media(media_aluminio, dias),
+                          media(media_chumbo, dias),
+                          media(media_estanho, dias),
+                          media(media_niquel, dias),
+                          media(media_dolar, dias))
 
             ultima_semana = semana_numero
-            semana_dias = 0
-            media_semana_zinco = 0
-            media_semana_cobre = 0
-            media_semana_aluminio = 0
-            media_semana_chumbo = 0
-            media_semana_estanho = 0
-            media_semana_niquel = 0
-            media_semana_dolar = 0
+            dias = 0
+            media_zinco = 0
+            media_cobre = 0
+            media_aluminio = 0
+            media_chumbo = 0
+            media_estanho = 0
+            media_niquel = 0
+            media_dolar = 0
 
         else:
-            semana_dias += 1
-            media_semana_zinco += float(price["zinco"])
-            media_semana_cobre += float(price["cobre"])
-            media_semana_aluminio += float(price["aluminio"])
-            media_semana_chumbo += float(price["chumbo"])
-            media_semana_estanho += float(price["estanho"])
-            media_semana_niquel += float(price["niquel"])
-            media_semana_dolar += float(price["dolar"])
+            dias += 1
+            media_zinco += float(price["zinco"])
+            media_cobre += float(price["cobre"])
+            media_aluminio += float(price["aluminio"])
+            media_chumbo += float(price["chumbo"])
+            media_estanho += float(price["estanho"])
+            media_niquel += float(price["niquel"])
+            media_dolar += float(price["dolar"])
 
-            table.add_row(f'{_data}', price["zinco"], price["cobre"], price["aluminio"], price["chumbo"],
+            table.add_row(f'{dia}', price["zinco"], price["cobre"],
+                          price["aluminio"], price["chumbo"],
                           price["estanho"], price["niquel"], price["dolar"])
 
     console = Console()
