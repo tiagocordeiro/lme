24e9ffba0de4505da3b020c418e967aeb032a12e
Tiago Cordeiro <tiago@mulhergorila.com>
2021-10-13 10:35:45 -0300

    Refactoring
    Melhora legibilidade do código e adequa a PEP8

--- tests/test_prices.py
+++ tests/test_prices.py
@@ -4,4 +4,4 @@ from lme.prices import get_prices
 def test_get_prices(capsys):
     get_prices()
     captured = capsys.readouterr()
-    assert f'Média Semana' in captured.out
+    assert 'Média Semana' in captured.out
