f37933b66d4391dbedcfa36c40771536a55fd89a
Tiago Cordeiro <tiago@mulhergorila.com>
2021-10-16 09:31:25 -0300

    BugFix - Quantidade de dias na semana corrigido
    This close #348

--- lme/prices.py
+++ lme/prices.py
@@ -7,7 +7,8 @@ from rich.table import Table
 
 
 def media(valor, dias):
-    return str(round(Decimal(valor / dias), ndigits=2))
+    _media = valor / dias
+    return str(round(Decimal(_media), ndigits=2))
 
 
 def get_prices():
@@ -23,17 +24,21 @@ def get_prices():
 
     table = Table(title=f"{title_icon} {title_text} {title_icon}")
 
-    table.add_column("Data", justify="center", style="cyan", no_wrap=True)
-    table.add_column("Zinco", justify="center", style="magenta")
-    table.add_column("Cobre", justify="center", style="red")
-    table.add_column("Alumínio", justify="center", style="blue")
-    table.add_column("Chumbo", justify="center", style="bright_cyan")
-    table.add_column("Estanho", justify="center", style="bright_blue")
-    table.add_column("Níquel", justify="center", style="bright_yellow")
-    table.add_column("Dolar", justify="center", style="green")
+    table.add_column("Data e semana", justify="center", style="cyan", no_wrap=True)
+    table.add_column("Zinco", justify="right", style="magenta")
+    table.add_column("Cobre", justify="right", style="red")
+    table.add_column("Alumínio", justify="right", style="blue")
+    table.add_column("Chumbo", justify="right", style="bright_cyan")
+    table.add_column("Estanho", justify="right", style="bright_blue")
+    table.add_column("Níquel", justify="right", style="bright_yellow")
+    table.add_column("Dolar", justify="right", style="green")
 
     ultima_semana = datetime.strptime(prices[0]["data"], "%Y-%m-%d").strftime(
         "%U")
+
+    semana_atual = datetime.strptime(prices[-1]["data"], "%Y-%m-%d").strftime(
+        "%U")
+
     dias = 0
     media_zinco = 0
     media_cobre = 0
@@ -45,20 +50,47 @@ def get_prices():
 
     for price in prices:
         dia = datetime.strptime(price["data"], "%Y-%m-%d").strftime("%d/%m/%Y")
-
         semana_numero = datetime.strptime(dia, "%d/%m/%Y").strftime("%U")
 
-        if semana_numero != ultima_semana:
-            table.add_row(f"Média Semana {int(semana_numero) - 1}",
-                          media(media_zinco, dias),
-                          media(media_cobre, dias),
-                          media(media_aluminio, dias),
-                          media(media_chumbo, dias),
-                          media(media_estanho, dias),
-                          media(media_niquel, dias),
-                          media(media_dolar, dias))
+        dias += 1
+        media_zinco += Decimal(price["zinco"])
+        media_cobre += Decimal(price["cobre"])
+        media_aluminio += Decimal(price["aluminio"])
+        media_chumbo += Decimal(price["chumbo"])
+        media_estanho += Decimal(price["estanho"])
+        media_niquel += Decimal(price["niquel"])
+        media_dolar += Decimal(price["dolar"])
 
-            ultima_semana = semana_numero
+        if semana_numero == ultima_semana:
+            table.add_row(f'{dia} - {semana_numero}', price["zinco"],
+                          price["cobre"],
+                          price["aluminio"], price["chumbo"],
+                          price["estanho"], price["niquel"], price["dolar"])
+
+            if dias == 4 and semana_numero == semana_atual:
+                table.add_row(f"[dim]Média Semana {semana_numero}",
+                              f"[dim]{media(media_zinco, dias)}",
+                              f"[dim]{media(media_cobre, dias)}",
+                              f"[dim]{media(media_aluminio, dias)}",
+                              f"[dim]{media(media_chumbo, dias)}",
+                              f"[dim]{media(media_estanho, dias)}",
+                              f"[dim]{media(media_niquel, dias)}",
+                              f"[dim]{media(media_dolar, dias)}")
+
+        else:
+            table.add_row(f"[dim]Média Semana {int(semana_numero) - 1}",
+                          f"[dim]{media(media_zinco, dias)}",
+                          f"[dim]{media(media_cobre, dias)}",
+                          f"[dim]{media(media_aluminio, dias)}",
+                          f"[dim]{media(media_chumbo, dias)}",
+                          f"[dim]{media(media_estanho, dias)}",
+                          f"[dim]{media(media_niquel, dias)}",
+                          f"[dim]{media(media_dolar, dias)}")
+            table.add_row(f'{dia} - {semana_numero}', price["zinco"],
+                          price["cobre"],
+                          price["aluminio"], price["chumbo"],
+                          price["estanho"], price["niquel"], price["dolar"])
+
             dias = 0
             media_zinco = 0
             media_cobre = 0
@@ -67,20 +99,7 @@ def get_prices():
             media_estanho = 0
             media_niquel = 0
             media_dolar = 0
-
-        else:
-            dias += 1
-            media_zinco += float(price["zinco"])
-            media_cobre += float(price["cobre"])
-            media_aluminio += float(price["aluminio"])
-            media_chumbo += float(price["chumbo"])
-            media_estanho += float(price["estanho"])
-            media_niquel += float(price["niquel"])
-            media_dolar += float(price["dolar"])
-
-            table.add_row(f'{dia}', price["zinco"], price["cobre"],
-                          price["aluminio"], price["chumbo"],
-                          price["estanho"], price["niquel"], price["dolar"])
+            ultima_semana = semana_numero
 
     console = Console()
     console.print(table, justify="center")
